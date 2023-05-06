814e17c21ad9ea4050c7ab7bdb26b91a5df1c5bb
kfujita <kfujita@student.42tokyo.jp>
2023-05-06 11:23:13 +0900

    `$$`を正常にParseできないバグを修正

--- srcs/serializer/_serializer_var.c
+++ srcs/serializer/_serializer_var.c
@@ -6,7 +6,7 @@
 /*   By: kfujita <kfujita@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 21:00:06 by kfujita           #+#    #+#             */
-/*   Updated: 2023/05/06 11:18:53 by kfujita          ###   ########.fr       */
+/*   Updated: 2023/05/06 11:20:44 by kfujita          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ bool	_serializer_var(const char **input, t_pars_mde *mode, t_cmd_elem *v)
 	if (*mode == M_VAR || *mode == M_DQUOTE_VAR)
 	{
 		v->type = CMDTYP_VARIABLE;
-		if (**input == '$')
+		if (**input == '$' && 0 < v->len)
 		{
 			v->nospace = true;
 			return (true);
