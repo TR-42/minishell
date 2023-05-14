b7e3e31cf4fbf7dd36f98e97be98c71b83546b61
kfujita <kfujita@student.42tokyo.jp>
2023-05-14 21:03:49 +0900

    create `build_cmd.c`

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: kfujita <kfujita@student.42tokyo.jp>       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/05/03 18:44:27 by kfujita           #+#    #+#              #
-#    Updated: 2023/05/14 19:36:17 by kfujita          ###   ########.fr        #
+#    Updated: 2023/05/14 20:42:44 by kfujita          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -18,6 +18,7 @@ SRCS_MAIN	= \
 SRCS_CHILDS	=\
 	_get_argc.c\
 	_one_elem_count.c\
+	build_cmd.c\
 	childs_dispose.c\
 	childs.c\
 	env_util.c\
--- srcs/childs/_build_cmd.h
+++ srcs/childs/_build_cmd.h
@@ -6,7 +6,7 @@
 /*   By: kfujita <kfujita@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 23:41:13 by kfujita           #+#    #+#             */
-/*   Updated: 2023/05/14 19:32:10 by kfujita          ###   ########.fr       */
+/*   Updated: 2023/05/14 20:48:35 by kfujita          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,7 @@
 
 int		_get_argc(const t_cmdelmarr *elmarr);
 size_t	_one_elem_count(const t_cmdelmarr *elemarr, size_t i_start);
+
+char	**build_cmd(t_cmdelmarr *elemarr, const char **envp);
 
 #endif
