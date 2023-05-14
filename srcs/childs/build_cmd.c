5aaa43e6a8ee0d7e4dcb3561f1d5bc68391d1524
kfujita <kfujita@student.42tokyo.jp>
2023-05-14 19:30:24 +0900

    `argv[i]`ごとのelem使用数を数える関数を分離

--- srcs/childs/build_cmd.c
+++ srcs/childs/build_cmd.c
@@ -6,7 +6,7 @@
 /*   By: kfujita <kfujita@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 00:34:59 by kfujita           #+#    #+#             */
-/*   Updated: 2023/05/11 23:57:39 by kfujita          ###   ########.fr       */
+/*   Updated: 2023/05/14 19:29:19 by kfujita          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,48 @@
 
 #include "_build_cmd.h"
 
+static size_t	_one_elem_count(const t_cmdelmarr *elemarr, size_t i_start)
+{
+	size_t		i;
+	t_cmd_elem	*elem;
+
+	elem = (t_cmd_elem *)(elemarr->p) + i_start;
+	i = 0;
+	if (is_cetyp_redirect(elem[i].type))
+	{
+		if (is_cetyp_var_or_normal(elem[++i].type))
+			i++;
+		return (i);
+	}
+	else if (elem[i].type == CMDTYP_PIPE)
+		return (1);
+	while ((i + i_start) < elemarr->len)
+	{
+		if (!is_cetyp_var_or_normal(elem[i].type))
+			break ;
+		i++;
+		if (!(elem[i - 1].nospace) || elem[i].type == CMDTYP_PIPE)
+			break ;
+	}
+	return (i);
+}
+
 int	_get_argc(const t_cmdelmarr *elemarr)
 {
 	size_t			argc;
 	size_t			i;
 	t_cmd_elem		*elem;
-	t_cmd_elem_type	t;
 
 	elem = (t_cmd_elem *)(elemarr->p);
 	i = 0;
 	argc = 0;
 	while (i < elemarr->len)
 	{
-		t = elem[i].type;
-		if (t == CMDTYP_PIPE)
-			break ;
-		if (t == CMDTYP_RED_IN || t == CMDTYP_RED_OUT || t == CMDTYP_RED_APPEND
-			|| t == CMDTYP_RED_HEREDOC)
-		{
-			t = elem[++i].type;
-			if (t == CMDTYP_NORMAL || t == CMDTYP_VARIABLE
-				|| t == CMDTYP_QUOTE_VAR)
-				i++;
-			continue ;
-		}
-		if (!(elem[i].nospace) || (i + 1) == elemarr->len
-			|| elem[i + 1].type == CMDTYP_PIPE)
+		if (is_cetyp_var_or_normal(elem[i].type))
 			argc++;
 		if (INT_MAX <= argc)
 			return (-1);
-		i++;
+		i += _one_elem_count(elemarr, i);
 	}
 	return (argc);
 }
