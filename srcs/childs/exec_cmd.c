61f15db73eb1e4cf685112b5a64a6938cfbdfc27
kfujita <kfujita@student.42tokyo.jp>
2023-05-15 00:21:11 +0900

    Update exec_cmd.c

--- srcs/childs/exec_cmd.c
+++ srcs/childs/exec_cmd.c
@@ -6,18 +6,66 @@
 /*   By: kfujita <kfujita@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 19:05:51 by kfujita           #+#    #+#             */
-/*   Updated: 2023/05/07 19:07:12 by kfujita          ###   ########.fr       */
+/*   Updated: 2023/05/14 23:41:48 by kfujita          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+// - close
+#include <unistd.h>
 
 // - exit
 #include <stdlib.h>
 
-#include "_childs.h"
+// - perror
+#include <stdio.h>
 
+// - dup2
+// - execve
+#include <unistd.h>
+
+#include "ft_printf/ft_printf.h"
+
+#include "_build_cmd.h"
+#include "_childs.h"
+#include "_filectrl_tools.h"
+
+static bool	_proc_redirect(t_ch_proc_info *info)
+{
+	(void)info;
+	return (true);
+}
+
+static void	dup2_and_close(int fd_dup_from, int fd_dup_to)
+{
+	dup2(fd_dup_from, fd_dup_to);
+	close(fd_dup_from);
+}
+
+// TODO: エラー時にFDを閉じる?
 noreturn void	exec_command(t_ch_proc_info *info_arr, size_t index)
 {
-	(void)info_arr;
-	(void)index;
-	exit(0);
+	char	**argv;
+	char	*exec_path;
+	int		ret;
+
+	if (!_proc_redirect(info_arr + index))
+		exit(1);
+	exec_path = NULL;
+	argv = build_cmd(info_arr[index].cmd, info_arr[index].envp);
+	ret = chk_and_get_fpath(argv[0], info_arr[index].envp, &exec_path);
+	if (ret != 0)
+	{
+		if (ret == CHK_GET_PATH_ERR_NOCMD)
+			ft_dprintf(STDERR_FILENO, "%s: Command not found", argv[0]);
+		else
+			perror(argv[0]);
+		dispose_proc_info_arr(info_arr);
+		exit(1);
+	}
+	dup2_and_close(info_arr[index].fd_to_this, STDIN_FILENO);
+	dup2_and_close(info_arr[index].fd_from_this, STDOUT_FILENO);
+	dispose_proc_info_arr(info_arr);
+	execve(exec_path, argv, info_arr[index].envp);
+	free(argv);
+	exit(1);
 }
