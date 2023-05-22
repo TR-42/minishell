ae32f97c35854cc80c33fabcbbc51c714a37dd2f
kfujita <kfujita@student.42tokyo.jp>
2023-05-22 23:47:44 +0900

    SIGINTで子プロセスを終了させた際に、exit_statusが130にならないバグを修正

--- srcs/childs/_exec_ch_proc_info_arr.c
+++ srcs/childs/_exec_ch_proc_info_arr.c
@@ -20,6 +20,22 @@
 
 #include "childs.h"
 
+// !! NO_ERROR
+__attribute__((nonnull))
+static bool	_is_end_and_get_stat(int *cpstat, t_cetyp cetype, bool is_signaled)
+{
+	if (is_signaled || !WIFEXITED(*cpstat))
+	{
+		*cpstat = 130;
+		return (true);
+	}
+	*cpstat = WEXITSTATUS(*cpstat);
+	if ((cetype == CMDTYP_OP_AND && *cpstat != EXIT_SUCCESS)
+		|| (cetype == CMDTYP_OP_OR && *cpstat == EXIT_SUCCESS))
+		return (true);
+	return (false);
+}
+
 // !! ERR_PRINTED
 // -> <inherit> pipe_fork_exec
 // (pipe_fork_execでエラー発生の場合、return 1)
@@ -30,10 +46,12 @@ int	_exec_ch_proc_info_arr(t_cprocinf *cparr, size_t cparr_len)
 	size_t	i_exec;
 	size_t	i_wait;
 	t_cetyp	cetype;
+	bool	is_signaled;
 
 	i_exec = 0;
 	i_wait = 0;
 	cpstat = 0;
+	is_signaled = false;
 	while (i_exec < cparr_len)
 	{
 		cetype = get_cmdterm(cparr[i_exec].cmd);
@@ -41,12 +59,11 @@ int	_exec_ch_proc_info_arr(t_cprocinf *cparr, size_t cparr_len)
 		if (cetype == CMDTYP_PIPE)
 			continue ;
 		while (i_wait < i_exec)
+		{
 			waitpid(cparr[i_wait++].pid, &cpstat, 0);
-		if (!WIFEXITED(cpstat))
-			return (130);
-		cpstat = WEXITSTATUS(cpstat);
-		if ((cetype == CMDTYP_OP_AND && cpstat != EXIT_SUCCESS)
-			|| (cetype == CMDTYP_OP_OR && cpstat == EXIT_SUCCESS))
+			is_signaled = (is_signaled || WIFSIGNALED(cpstat));
+		}
+		if (_is_end_and_get_stat(&cpstat, cetype, is_signaled))
 			break ;
 	}
 	return (cpstat);
