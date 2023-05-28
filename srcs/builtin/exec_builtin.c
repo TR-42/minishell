38874e8d8704ed98c8f8c1a07f3daf5edaa9aa13
kitsuki <IT.King5963@gmail.com>
2023-05-28 20:15:52 +0900

    change strlen to sizeof

--- srcs/builtin/exec_builtin.c
+++ srcs/builtin/exec_builtin.c
@@ -6,7 +6,7 @@
 /*   By: kitsuki <kitsuki@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/28 17:53:48 by kitsuki           #+#    #+#             */
-/*   Updated: 2023/05/28 20:04:20 by kitsuki          ###   ########.fr       */
+/*   Updated: 2023/05/28 20:15:32 by kitsuki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,22 +25,22 @@ int	exec_builtin(char **argv, int *exit_status)
 {
 	if (argv == NULL || *argv == NULL)
 		return (-1);
-	if (ft_strncmp(*argv, CD, ft_strlen(CD) + 1) == 0)
+	if (ft_strncmp(*argv, CD, sizeof(CD)) == 0)
 		*exit_status = builtin_cd(argv);
-	else if (ft_strncmp(*argv, ECHO, ft_strlen(ECHO) + 1) == 0)
+	else if (ft_strncmp(*argv, ECHO, sizeof(ECHO)) == 0)
 		*exit_status = builtin_echo(argv);
-	else if (ft_strncmp(*argv, ENV, ft_strlen(ENV) + 1) == 0)
+	else if (ft_strncmp(*argv, ENV, sizeof(ENV)) == 0)
 		*exit_status = builtin_env(argv);
-	else if (ft_strncmp(*argv, EXIT, ft_strlen(EXIT) + 1) == 0)
+	else if (ft_strncmp(*argv, EXIT, sizeof(EXIT)) == 0)
 	{
 		*exit_status = builtin_exit(argv, *exit_status);
 		return (-1);
 	}
-	else if (ft_strncmp(*argv, EXPORT, ft_strlen(EXPORT) + 1) == 0)
+	else if (ft_strncmp(*argv, EXPORT, sizeof(EXPORT)) == 0)
 		*exit_status = builtin_export(argv);
-	else if (ft_strncmp(*argv, PWD, ft_strlen(PWD) + 1) == 0)
+	else if (ft_strncmp(*argv, PWD, sizeof(PWD)) == 0)
 		*exit_status = builtin_pwd();
-	else if (ft_strncmp(*argv, UNSET, ft_strlen(UNSET) + 1) == 0)
+	else if (ft_strncmp(*argv, UNSET, sizeof(UNSET)) == 0)
 		*exit_status = builtin_unset(argv);
 	else
 		return (0);
