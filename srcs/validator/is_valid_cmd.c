64f7ec2acd66ad61a4293be891391f53c9195d97
kfujita <kfujita@student.42tokyo.jp>
2023-06-02 21:37:49 +0900

    termのinval chkを関数分離

--- srcs/validator/is_valid_cmd.c
+++ srcs/validator/is_valid_cmd.c
@@ -15,27 +15,47 @@
 #include "_build_cmd.h"
 
 // !! NO_ERROR
+static bool	_is_term_inval_chk(
+	const t_cmd_elem *elems,
+	bool is_last_cmd,
+	size_t i,
+	t_cmd_inval_typ *result
+)
+{
+	if (is_cetyp_terminator(elems[i].type))
+	{
+		if (is_last_cmd)
+		{
+			*result = CMD_INVAL_OP_NOPAIR;
+			return (true);
+		}
+		else if (i == 0)
+		{
+			*result = CMD_INVAL_NOCMD;
+			return (true);
+		}
+	}
+	return (false);
+}
+
+// !! NO_ERROR
 __attribute__((nonnull))
 static t_cmd_inval_typ	_is_valid_elem(
 	const t_cmdelmarr *cmdelemarr,
 	const t_cmd_elem *elems,
 	bool is_last_cmd)
 {
-	size_t		i;
-	size_t		tmp;
-	bool		is_cmd_shown;
+	size_t			i;
+	size_t			tmp;
+	bool			is_cmd_shown;
+	t_cmd_inval_typ	inval_typ;
 
 	i = 0;
 	is_cmd_shown = false;
 	while (i < cmdelemarr->len)
 	{
-		if (is_cetyp_terminator(elems[i].type))
-		{
-			if (is_last_cmd)
-				return (CMD_INVAL_OP_NOPAIR);
-			else if (i == 0)
-				return (CMD_INVAL_NOCMD);
-		}
+		if (_is_term_inval_chk(elems, is_last_cmd, i, &inval_typ))
+			return (inval_typ);
 		tmp = _one_elem_count(cmdelemarr, i);
 		if (!is_cetyp_redirect(elems[i].type))
 			is_cmd_shown = true;
