447a98dd8198a1a9a47722966a9f108b7353a109
kitsuki <IT.King5963@gmail.com>
2023-06-10 23:49:17 +0900

    save 01 fix_pwd

--- srcs/builtin/cd.c
+++ srcs/builtin/cd.c
@@ -6,7 +6,7 @@
 /*   By: kitsuki <kitsuki@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 17:04:37 by kitsuki           #+#    #+#             */
-/*   Updated: 2023/06/03 18:58:34 by kitsuki          ###   ########.fr       */
+/*   Updated: 2023/06/10 23:39:33 by kitsuki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,8 @@ int	builtin_cd(char **argv)
 
 	if (*(++argv) == NULL)
 		return (print_error(COMMAND, NULL, FEWARGERR, 1));
+	else if (*(argv + 1) != NULL)
+		return (print_error(COMMAND, NULL, MANYARGERR, 1));
 	ft_bzero(oldpath, PATH_MAX + 1);
 	ft_strlcpy(oldpath, OLDPWD, sizeof(OLDPWD));
 	if (getcwd(oldpath + sizeof(OLDPWD) - 1, PATH_MAX) == NULL)
@@ -47,4 +49,23 @@ int	builtin_cd(char **argv)
 	if (!set_environ(oldpath))
 		return (print_error(COMMAND, NULL, SETENVERR, 1));
 	return (0);
+}
+
+static bool	save_pwd(void)
+{
+	char	path[PATH_MAX + 1];
+	char	*tmp;
+
+	ft_bzero(path, PATH_MAX + 1);
+	if (getcwd(path, PATH_MAX) != NULL)
+		return (0);
+	tmp = search_environ(PWD);
+	if (tmp != NULL)
+	{
+		if (ft_putstr_fd_with_err(*tmp, STDOUT_FILENO) == 0
+			|| ft_putstr_fd_with_err("\n", STDOUT_FILENO) == 0)
+			return (print_error(COMMAND, NULL, PRINTFERR, 1));
+		return (0);
+	}
+	return (print_error(COMMAND, NULL, FAILERR, 1));
 }
--- srcs/builtin/pwd.c
+++ srcs/builtin/pwd.c
@@ -6,10 +6,11 @@
 /*   By: kitsuki <kitsuki@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 17:16:51 by kitsuki           #+#    #+#             */
-/*   Updated: 2023/05/28 19:35:54 by kitsuki          ###   ########.fr       */
+/*   Updated: 2023/06/10 23:33:29 by kitsuki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "_environ.h"
 #include "_util_commands.h"
 #include "ft_printf/ft_printf.h"
 #include "ft_mem/ft_mem.h"
@@ -24,10 +25,12 @@
 # define PATH_MAX MAXPATHLEN
 #endif
 #define COMMAND "pwd"
+#define PWD		"PWD"
 
 int	builtin_pwd(void)
 {
 	char	path[PATH_MAX + 1];
+	char	*tmp;
 
 	ft_bzero(path, PATH_MAX + 1);
 	if (getcwd(path, PATH_MAX) != NULL)
@@ -37,5 +40,13 @@ int	builtin_pwd(void)
 			return (print_error(COMMAND, NULL, PRINTFERR, 1));
 		return (0);
 	}
-	return (print_error(COMMAND, NULL, NULL, errno));
+	tmp = search_environ(PWD);
+	if (tmp != NULL)
+	{
+		if (ft_putstr_fd_with_err(*tmp, STDOUT_FILENO) == 0
+			|| ft_putstr_fd_with_err("\n", STDOUT_FILENO) == 0)
+			return (print_error(COMMAND, NULL, PRINTFERR, 1));
+		return (0);
+	}
+	return (print_error(COMMAND, NULL, FAILERR, 1));
 }
